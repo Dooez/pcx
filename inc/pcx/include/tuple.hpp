c18c67e5b5deeab857cb1b4442b3d5e4540a7b14
Timofey <dooezzz@gmail.com>
2024-11-06 22:02:59 +0300

    WIP(group_invoke): basic concepts.

--- inc/pcx/include/tuple.hpp
+++ inc/pcx/include/tuple.hpp
@@ -112,7 +112,32 @@ PCX_AINLINE auto make_ituple(Args&&... args) {
     return ituple_t(static_cast<Args&&>(args)...);
 }
 
+namespace detail_ {
+template<uZ, uZ, typename...>
+struct index_into_types_h;
+template<uZ K, uZ I, typename T, typename... Ts>
+struct index_into_types_h<K, I, T, Ts...> {
+    using type = std::conditional_t<K == I, T, typename index_into_types_h<K + 1, I, Ts...>::type>;
+};
+template<uZ K, typename T>
+struct index_into_types_h<K, K, T> {
+    using type = T;
+};
+template<uZ I, typename... Ts>
+    requires(I < sizeof...(Ts))
+struct index_into_types {
+    using type = index_into_types_h<0, I, Ts...>::type;
+};
+
+};    // namespace detail_
 }    // namespace pcx::h
+
+template<typename... Ts>
+struct std::tuple_size<pcx::h::ituple<Ts...>> : std::integral_constant<std::size_t, sizeof...(Ts)> {};
+template<std::size_t I, typename... Ts>
+struct std::tuple_element<I, pcx::h::ituple<Ts...>> {
+    using type = pcx::h::detail_::index_into_types<I, Ts...>::type;
+};
 
 namespace pcx::i {
 template<typename... Ts>
@@ -130,11 +155,65 @@ PCX_AINLINE auto get(T&& tuple) {
     return std::get<I>(std::forward<T>(tuple));
 }
 
+using std::tuple_element;
+using std::tuple_element_t;
+using std::tuple_size;
+using std::tuple_size_v;
+
 template<uZ N>
 struct multi_stage_op {
     static constexpr uZ stage_count = N;
 };
 
+namespace detail_ {
+template<typename>
+struct is_tuple : std::false_type {};
+template<typename... Ts>
+struct is_tuple<tuple<Ts...>> : std::true_type {};
+template<typename T>
+inline constexpr auto is_tuple_v = is_tuple<T>::value;
+
+template<typename...>
+struct are_same_size_tuples : std::false_type {};
+template<typename T>
+    requires(is_tuple_v<T>)
+struct are_same_size_tuples<T> : std::true_type {};
+template<typename T, typename... Ts>
+    requires(is_tuple_v<T> && (is_tuple_v<Ts> && ...))
+struct are_same_size_tuples<T, Ts...> {
+    static constexpr bool value = ((tuple_size_v<T> == tuple_size_v<Ts>) && ...);
+};
+template<typename... T>
+inline constexpr auto are_same_size_tuples_v = are_same_size_tuples<T...>::value;
+
+template<uZ I, typename F, typename... Args>
+struct is_group_invokable_h {
+    static constexpr bool value =
+        std::invocable<F, tuple_element_t<I, Args>...> && is_group_invokable_h<I - 1, F, Args...>::value;
+};
+template<typename F, typename... Args>
+struct is_group_invokable_h<0, F, Args...> {
+    static constexpr bool value = std::invocable<F, tuple_element_t<0, Args>...>;
+};
+template<typename F, typename... Args>
+struct is_group_invokable {
+    static constexpr bool value = is_group_invokable_h<(..., tuple_size_v<Args>)-1, F, Args...>::value;
+};
+
+}    // namespace detail_
+
+template<typename T>
+concept any_tuple = detail_::is_tuple_v<T>;
+template<typename... Ts>
+concept same_size_tuples = (any_tuple<Ts> && ...) && detail_::are_same_size_tuples_v<Ts...>;
+template<typename F, typename... Args>
+concept group_invokable = same_size_tuples<Args...> && detail_::is_group_invokable<F, Args...>::value;
+
+template<typename F, typename... Args>
+    requires group_invokable<F, Args...>
+constexpr auto group_invoke(F&& f, Args&&... args);
+
 
 }    // namespace pcx::i
+
 #undef PCX_AINLINE
