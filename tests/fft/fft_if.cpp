47e8baf171aad9450ad2a4fb3716cbec20e42b4b
Timofey <dooezzz@gmail.com>
2025-05-13 22:19:49 +0300

    Update testing with interface.

--- tests/fft/fft_if.cpp
+++ tests/fft/fft_if.cpp
@@ -1,40 +1,34 @@
+#include "common.hpp"
 #include "pcx/fft.hpp"
 
 #include <print>
-
-template<typename T>
-struct std::formatter<std::complex<T>, char> {
-    std::formatter<T, char> m_fmt;
-    template<class ParseContext>
-    constexpr auto parse(ParseContext& ctx) -> ParseContext::iterator {
-        return m_fmt.parse(ctx);
-    }
-
-    template<class FmtContext>
-    FmtContext::iterator format(std::complex<T> cxv, FmtContext& ctx) const {
-        *ctx.out() = "(";
-        auto out   = m_fmt.format(cxv.real(), ctx);
-        *(out++)   = ",";
-        *out       = " ";
-        out        = m_fmt.format(cxv.imag(), ctx);
-        *(out++)   = ")";
-        return out;
-    }
-};
 namespace stdv = std::views;
 namespace stdr = std::ranges;
+
+using pcx::f32;
+using pcx::f64;
+using pcx::uZ;
 
 int main() {
     size_t fft_size = 2;
 
-    auto fft = pcx::fft_plan<float>(fft_size);
-    auto dat = std::vector<std::complex<float>>(fft_size);
-    for (auto [i, v]: stdv::enumerate(dat)) {
-        v = std::exp(std::complex<float>{0, static_cast<float>(i)} * 2.F * std::numbers::pi_v<float> / 2.F);
-    }
-    fft.fft(dat);
-    for (auto v: dat) {
-        std::print("{:.2f} ", abs(v));
+    while (fft_size < 2048 * 2048) {
+        auto fft  = pcx::fft_plan<float>(fft_size);
+        auto data = std::vector<std::complex<float>>(fft_size);
+        for (auto [i, v]: stdv::enumerate(data)) {
+            v = std::exp(std::complex<float>{0, static_cast<float>(i)}    //
+                         * 2.F                                            //
+                         * std::numbers::pi_v<float>                      //
+                         / 2.F);
+        }
+        auto data_c = data;
+        fft.fft(data);
+        pcx::testing::naive_fft(data_c, 16, 8);
+        pcx::testing::check_correctness(data_c, data, 16, 8, true, true, true);
+        // for (auto v: data) {
+        //     std::print("{:.2f} ", abs(v));
+        // }
+        fft_size *= 2;
     }
 
     return 0;
