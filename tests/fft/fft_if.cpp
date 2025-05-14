a617515b7ddb226432043f78d8153b7c3d3a4727
Timofey <dooezzz@gmail.com>
2025-05-14 20:17:59 +0300

    Sequential basic checks pass.

--- tests/fft/fft_if.cpp
+++ tests/fft/fft_if.cpp
@@ -9,16 +9,17 @@ using pcx::f32;
 using pcx::f64;
 using pcx::uZ;
 
+template<typename fX>
 bool check_br(uZ fft_size) {
     constexpr auto ops = pcx::fft_options{.pt = pcx::fft_permutation::bit_reversed};
-    auto           fft = pcx::fft_plan<float, ops>(fft_size);
+    auto           fft = pcx::fft_plan<fX, ops>(fft_size);
 
-    auto data = std::vector<std::complex<float>>(fft_size);
+    auto data = std::vector<std::complex<fX>>(fft_size);
     for (auto [i, v]: stdv::enumerate(data)) {
-        v = std::exp(std::complex<float>{0, static_cast<float>(i)}    //
-                     * 2.F                                            //
-                     * std::numbers::pi_v<float>                      //
-                     / 2.F);
+        v = std::exp(std::complex<fX>{0, static_cast<fX>(i)}    //
+                     * static_cast<fX>(2.)                      //
+                     * std::numbers::pi_v<fX>                   //
+                     / static_cast<fX>(2.));
     }
 
     auto data_c = data;
@@ -35,16 +36,17 @@ bool check_br(uZ fft_size) {
     return true;
 }
 
+template<typename fX>
 bool check_normal(uZ fft_size) {
     constexpr auto ops = pcx::fft_options{.pt = pcx::fft_permutation::normal};
-    auto           fft = pcx::fft_plan<float, ops>(fft_size);
+    auto           fft = pcx::fft_plan<fX, ops>(fft_size);
 
-    auto data = std::vector<std::complex<float>>(fft_size);
+    auto data = std::vector<std::complex<fX>>(fft_size);
     for (auto [i, v]: stdv::enumerate(data)) {
-        v = std::exp(std::complex<float>{0, static_cast<float>(i)}    //
-                     * 2.F                                            //
-                     * std::numbers::pi_v<float>                      //
-                     / 2.F);
+        v = std::exp(std::complex<fX>{0, static_cast<fX>(i)}    //
+                     * static_cast<fX>(2.)                      //
+                     * std::numbers::pi_v<fX>                   //
+                     / static_cast<fX>(2.));
     }
 
     auto data_c = data;
@@ -63,16 +65,17 @@ bool check_normal(uZ fft_size) {
     return true;
 }
 
+template<typename fX>
 bool check_shiftd(uZ fft_size) {
     constexpr auto ops = pcx::fft_options{.pt = pcx::fft_permutation::shifted};
-    auto           fft = pcx::fft_plan<float, ops>(fft_size);
+    auto           fft = pcx::fft_plan<fX, ops>(fft_size);
 
-    auto data = std::vector<std::complex<float>>(fft_size);
+    auto data = std::vector<std::complex<fX>>(fft_size);
     for (auto [i, v]: stdv::enumerate(data)) {
-        v = std::exp(std::complex<float>{0, static_cast<float>(i)}    //
-                     * 2.F                                            //
-                     * std::numbers::pi_v<float>                      //
-                     / 2.F);
+        v = std::exp(std::complex<fX>{0, static_cast<fX>(i)}    //
+                     * static_cast<fX>(2.)                      //
+                     * std::numbers::pi_v<fX>                   //
+                     / static_cast<fX>(2.));
     }
 
     auto data_c = data;
@@ -92,13 +95,23 @@ bool check_shiftd(uZ fft_size) {
 }
 
 int main() {
-    size_t fft_size = 4;
+    size_t fft_size = 2;
     while (fft_size < 2048 * 2048) {
-        if (!check_br(fft_size))
+        if (!check_br<f32>(fft_size))
             return -1;
-        if (!check_normal(fft_size))
+        if (!check_normal<f32>(fft_size))
             return -1;
-        if (!check_shiftd(fft_size))
+        if (!check_shiftd<f32>(fft_size))
+            return -1;
+        fft_size *= 2;
+    }
+    fft_size = 2;
+    while (fft_size < 2048 * 2048) {
+        if (!check_br<f64>(fft_size))
+            return -1;
+        if (!check_normal<f64>(fft_size))
+            return -1;
+        if (!check_shiftd<f64>(fft_size))
             return -1;
         fft_size *= 2;
     }
